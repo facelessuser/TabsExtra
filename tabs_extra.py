4531eb0b613bbf99363aa1a40dc960b08ea2404e
facelessuser <faceless.shop@gmail.com>
2019-11-29 08:39:53 -0700

    Fix issue where commands were not properly being overridden

--- tabs_extra.py
+++ tabs_extra.py
@@ -568,7 +568,6 @@ class TabsExtraListener(sublime_plugin.EventListener):
             if extra_command_call and command_name == "close_file":
                 view.settings().set("tabs_extra_closing", True)
                 return cmd
-            command_name = "tabs_extra_close"
             group, index = window.get_view_index(view)
             args = {"group": group, "index": index}
         if command_name in ["close_by_index", "close"]:
